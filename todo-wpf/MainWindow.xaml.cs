a76a44cc33095de70f5542b01964e80b122be356
udon247 <57240541+udon247@users.noreply.github.com>
2024-11-26 00:51:50 +0900

    単純な Todo 機能を追加

--- todo-wpf/MainWindow.xaml.cs
+++ todo-wpf/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace todo_wpf
 {
@@ -19,6 +10,15 @@ namespace todo_wpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(content.Text))
+            {
+                memoList.Items.Add(content.Text);
+                content.Clear();
+            }
         }
     }
 }
